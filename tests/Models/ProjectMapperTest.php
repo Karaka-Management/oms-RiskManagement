4453ca5cdc6bbd499998a8fd48d62aa5198fc8d4
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-26 03:03:31 +0000

    fix tests

--- tests/Models/ProjectMapperTest.php
+++ tests/Models/ProjectMapperTest.php
@@ -17,6 +17,13 @@ namespace Modules\RiskManagement\tests\Models;
 use Modules\ProjectManagement\Models\NullProject;
 use Modules\RiskManagement\Models\Project;
 use Modules\RiskManagement\Models\ProjectMapper;
+use Modules\Admin\Models\NullAccount;
+use Modules\Media\Models\Media;
+use Modules\ProjectManagement\Models\ProgressType;
+use Modules\Tasks\Models\Task;
+use Modules\ProjectManagement\Models\Project as PMProject;
+use Modules\ProjectManagement\Models\ProjectMapper as PMProjectMapper;
+use phpOMS\Stdlib\Base\FloatInt;
 
 /**
  * @internal
@@ -29,8 +36,49 @@ final class ProjectMapperTest extends \PHPUnit\Framework\TestCase
      */
     public function testCRUD() : void
     {
+        $project = new PMProject();
+
+        $project->setName('Projectname');
+        $project->description = 'Description';
+        $project->createdBy   = new NullAccount(1);
+        $project->start       = new \DateTime('2000-05-05');
+        $project->end         = new \DateTime('2005-05-05');
+
+        $money = new FloatInt();
+        $money->setString('1.23');
+
+        $project->budgetCosts             = $money;
+        $project->budgetEarnings          = $money;
+        $project->actualCosts             = $money;
+        $project->actualEarnings          = $money;
+
+        $task        = new Task();
+        $task->title = 'ProjectTask 1';
+        $task->setCreatedBy(new NullAccount(1));
+
+        $task2        = new Task();
+        $task2->title = 'ProjectTask 2';
+        $task2->setCreatedBy(new NullAccount(1));
+
+        $project->addTask($task);
+        $project->addTask($task2);
+
+        $project->progress = 10;
+        $project->setProgressType(ProgressType::TASKS);
+
+        $media              = new Media();
+        $media->createdBy   = new NullAccount(1);
+        $media->description = 'desc';
+        $media->setPath('some/path');
+        $media->size      = 11;
+        $media->extension = 'png';
+        $media->name      = 'Project Media';
+        $project->addMedia($media);
+
+        $id = PMProjectMapper::create()->execute($project);
+
         $obj              = new Project();
-        $obj->project     = new NullProject(1);
+        $obj->project     = new NullProject($id);
         $obj->responsible = 1;
         $obj->deputy      = 1;
 
